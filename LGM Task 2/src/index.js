a808f9f0b5d97ffced96932df6f2d5c7f6506f57
Sumedh Madhav Phade <70339788+Sumedh-byte@users.noreply.github.com>
2021-11-28 16:04:05 +0530

    LGMVIP Task 2

---
+++ LGM Task 2/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from "./Card";
+
+ReactDOM.render(
+  <div><Card/>
+  </div>,
+  
+  document.getElementById('root')
+);
+
+
+
